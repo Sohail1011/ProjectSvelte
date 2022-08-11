a91df38ff009335af196b53ad939eb0342e1ec56
Sohail1011 <aratavalga@gmail.com>
2022-08-11 12:19:05 -0500

    svelte practice

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<main>
+  <h1>Menu</h1>
+</main>
+
+<style>
+  h1{
+    font-size: 10em;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+  }
+</style>
